581a863e60627b4b0ad31acfddb03199cc6015fc
Daksh <gargdaksh0616@gmail.com>
2026-05-31 14:47:54 +0530

    Merge conflict fix

--- src/main.cpp
+++ src/main.cpp
@@ -8,30 +8,37 @@
 #include <glm/gtc/type_ptr.hpp>
 
 #include <Shader.h>
-#include<Camera.h>
-
 
 #include <iostream>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
-void mouse_callback(GLFWwindow* window, double xpos, double ypos);
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void processInput(GLFWwindow *window);
 
 const unsigned int SCR_WIDTH = 1920 ;
 const unsigned int SCR_HEIGHT = 1080;
 
+<<<<<<< HEAD
 Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 float lastX = SCR_WIDTH/2.0f;
 float lastY = SCR_HEIGHT/2.0f;
 bool firstMouse = true;
+=======
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+>>>>>>> parent of 3d8fa7e (Merge branch 'working' of https://github.com/DaDicaso/Camera_Work_OpenGL into working)
 
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
+<<<<<<< HEAD
 glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 
 int main(){
+=======
+int main()
+{
+>>>>>>> parent of 3d8fa7e (Merge branch 'working' of https://github.com/DaDicaso/Camera_Work_OpenGL into working)
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -48,10 +55,13 @@ int main(){
 
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+<<<<<<< HEAD
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
 
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED); 
+=======
+>>>>>>> parent of 3d8fa7e (Merge branch 'working' of https://github.com/DaDicaso/Camera_Work_OpenGL into working)
 
     if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
         std::cout << "Failed to initialize GLAD" << std::endl;
@@ -136,6 +146,7 @@ int main(){
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+<<<<<<< HEAD
     
     while(!glfwWindowShouldClose(window)){
         // per frame logic
@@ -144,6 +155,70 @@ int main(){
         lastFrame = currentFrame;
 
         // Input
+=======
+    // set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // load image, create texture and generate mipmaps
+    int width, height, nrChannels;
+    stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip laoded image texture on the y-axis.
+    // The FileSystem:
+    unsigned char *data = stbi_load("/home/dicaso/Desktop/TheOne/OpenGL/texs/container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    // Texture 2
+    glGenTextures(1, &texture2);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    // Set the texutre wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    // Set texture filtering parameters
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // load image, create texture and generate mipmaps
+    data = stbi_load("/home/dicaso/Desktop/TheOne/OpenGL/texs/awesomeface.png", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    ourShader.use(); // Activates the shader g(glGetUnif
+    // Setting up the uniforms manually
+    glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+    // Setting up the uniforms via the texutre class
+    ourShader.setInt("texture2", 1);
+
+    // glm::mat4 view = glm::mat4(1.0f);
+    // view = glm::translate(view,  glm::vec3(0.0f, 0.0f, -3.0f));
+
+    glm::mat4 projection = glm::mat4(1.0f);
+    projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+    ourShader.setMat4("projection", projection);
+
+    // Render Loop
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+>>>>>>> parent of 3d8fa7e (Merge branch 'working' of https://github.com/DaDicaso/Camera_Work_OpenGL into working)
         processInput(window);
 
         // Render
@@ -166,6 +241,7 @@ int main(){
         lightingShader.setVec3("light.diffuse", diffuseColor);
         lightingShader.setVec3("light.specualar", 1.0f, 1.0f, 1.0f);
 
+<<<<<<< HEAD
         // Material Properties
         lightingShader.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
@@ -177,6 +253,21 @@ int main(){
         glm::mat4 view = camera.GetViewMatrix();
         lightingShader.setMat4("projection", projection);
         lightingShader.setMat4("view", view);
+=======
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // create transformations
+        glm::mat4 view = glm::mat4(1.0f);
+        const float radius = 10.0f;
+        float camX = static_cast<float>(sin(glfwGetTime()) * radius);
+        float camZ = static_cast<float>(cos(glfwGetTime()) * radius);
+
+        view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+        ourShader.setMat4("view", view);
+>>>>>>> parent of 3d8fa7e (Merge branch 'working' of https://github.com/DaDicaso/Camera_Work_OpenGL into working)
 
         // World transformations
         glm::mat4 model = glm::mat4(1.0f);
@@ -209,6 +300,7 @@ int main(){
     return 0;
 }
 
+<<<<<<< HEAD
 void processInput(GLFWwindow *window){
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
@@ -248,4 +340,24 @@ void mouse_callback(GLFWwindow *window, double xposIn, double yposIn){
 
 void scroll_callback(GLFWwindow *window, double xoffset, double yoffset){
     camera.ProcessMouseScroll(static_cast<float>(yoffset));
+=======
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+    const float cameraSpeed = 2.5f * deltaTime;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+>>>>>>> parent of 3d8fa7e (Merge branch 'working' of https://github.com/DaDicaso/Camera_Work_OpenGL into working)
 }
